a4c831d5071ac8f29a0f86398c5c30dbc9c7b4fd
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-05-17 17:07:26 -0400

    commit tree 44873a01510766c2d931d9ee726165478a38b433

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+//let feet;
+function scuberGreetingForFeet(feet){
+  if (feet  <= 400) {
+   return 'This one is on me!';
+   } else if (feet <= 2001){
+   return "I will gladly take your thirty bucks.";
+   } else    (feet > 2500)
+   return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === 'NYC' ? "Ok, sounds good." : "No go." );
 }
+// function ternaryCheckCity(city){
+// return ternaryCheckCity = city === "NYC" ? "Ok, sounds good." : "No go." ;
+// }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tipAmount,){
   // Write your code here!
+  switch(tipAmount) {
+    case 'generous':
+      return"Thank you so much.";
+      break;
+    case 'not as generous':
+      return 'Thank you.' ;
+      break;
+    default:
+      return "Bye.";
+      // code block
+  }
 }
